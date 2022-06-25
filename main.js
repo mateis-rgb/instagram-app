bf68c8594abe412b1e59b0a919f89240f6831c6b
Mateis <54773138+mateis-rgb@users.noreply.github.com>
2022-06-25 10:44:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+// const path = require('path')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      devTools: false,
+    }
+  })
+  mainWindow.loadURL('https://www.instagram.com/')
+}
+
+// Cette méthode sera appelée quand Electron aura fini
+// de s'initialiser et sera prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quant cet événement est émit.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
